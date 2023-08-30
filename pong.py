d81965cd71ac6f60ee5cc28ca029441025814908
MoonS <msuyoung5@gmail.com>
2023-08-30 22:58:45 +0800

    Update pong.py

--- pong.py
+++ pong.py
@@ -5,85 +5,66 @@ pygame.init()
 WIDTH, HEIGHT = 640, 480
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Pong')
+clock = pygame.time.Clock()
 
 class Paddle:
-    COLOUR = (255, 255, 255)
-    VELOCITY = 5
-    def __init__(self, x, y, width, height):
+    def __init__(self, screen, x, y):
+        self.screen = screen
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+        self.velocity = 5
 
-    def create(self, screen):
-        pygame.draw.rect(screen, self.COLOUR, (self.x, self.y, self.width, self.height))
-
-    def move(self, up=True):
+    def draw(self):
+        pygame.draw.rect(self.screen, (255 ,255, 255), (self.x, self.y, 10, 80))
+    
+    def move(self, up):
         if up:
-            self.y -= self.VELOCITY
+            self.y -= self.velocity
         else:
-            self.y += self.VELOCITY
+            self.y += self.velocity
 
 class Ball:
-    COLOUR = (255, 255, 255)
-    MAX_VELOCITY = 5
-    def __init__(self, x, y, radius):
+    def __init__(self, screen, x, y, radius):
+        self.screen = screen
         self.x = x
         self.y = y
-        self.radius = radius
-        self.x_vel = self.MAX_VELOCITY
+        self.x_vel = 7
         self.y_vel = 0
+        self.radius = radius
 
-    def create(self, screen):
-        pygame.draw.circle(screen, self.COLOUR, (self.x, self.y), self.radius)
+    def draw(self):
+        pygame.draw.circle(self.screen, (255 ,255, 255), (self.x, self.y), self.radius)
 
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
 
-def draw(screen, paddles, ball):
-    screen.fill((0, 0, 0))
-
-    for paddle in paddles:
-        paddle.create(screen)
-
-    ball.create(screen)
-
-def collision(ball, left_paddle, right_paddle):
-    if ball.y + ball.radius <= 0 or ball.y + ball.radius >= HEIGHT:
-        ball.y_vel *= -1
-
-    if ball.x_vel < 0:
-        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
-            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
-                ball.x_vel *= -1
-
-    else:
-        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
-            if ball.x + ball.radius >= right_paddle.x:
-                ball.x_vel *= -1
-
-def paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_w] and left_paddle.y - left_paddle.VELOCITY >= 0:
+def move_paddle(input, left_paddle, right_paddle):
+    if input[pygame.K_w] and left_paddle.y - left_paddle.velocity >= 0:
         left_paddle.move(up=True)
-    elif keys[pygame.K_s] and left_paddle.y + left_paddle.height + left_paddle.VELOCITY <= HEIGHT: 
+    elif input[pygame.K_s] and left_paddle.y + 80 + left_paddle.velocity <= HEIGHT: 
         left_paddle.move(up=False)
 
-    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VELOCITY >= 0:
+    if input[pygame.K_UP] and right_paddle.y - right_paddle.velocity >= 0:
         right_paddle.move(up=True)
-    elif keys[pygame.K_DOWN] and right_paddle.y + right_paddle.height + right_paddle.VELOCITY <= HEIGHT:
+    elif input[pygame.K_DOWN] and right_paddle.y + 80 + right_paddle.velocity <= HEIGHT:
         right_paddle.move(up=False)
 
-def main():
-    clock = pygame.time.Clock()
+def hit_paddle(ball, left_paddle, right_paddle):
+    if ball.x_vel < 0 and ball.x - ball.radius <= left_paddle.x + 10 and left_paddle.y <= ball.y <= left_paddle.y + 80:
+        ball.x_vel *= -1
+    elif ball.x_vel > 0 and ball.x + ball.radius >= right_paddle.x and right_paddle.y <= ball.y <= right_paddle.y + 80:
+        ball.x_vel *= -1
 
-    left_paddle = Paddle(10, HEIGHT//2 - 50, 20, 100)
-    right_paddle = Paddle(WIDTH - 30, HEIGHT//2 - 50, 20, 100)
-    ball = Ball(WIDTH//2, HEIGHT//2, 7)
+    if ball.y - ball.radius <= 0 or ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+
+def main():
+    left_paddle = Paddle(SCREEN, 15, HEIGHT//2 - 40)
+    right_paddle = Paddle(SCREEN, WIDTH - 25, HEIGHT//2 - 40)
+    ball = Ball(SCREEN, WIDTH//2, HEIGHT//2, 7)
 
     while True:
-        draw(SCREEN, [left_paddle, right_paddle], ball)
-
         clock.tick(60)
         pygame.display.update()
 
@@ -92,11 +73,17 @@ def main():
                 pygame.quit()
                 exit()
 
-        keys = pygame.key.get_pressed()
-        paddle_movement(keys, left_paddle, right_paddle)
+        SCREEN.fill((0, 0, 0))
+        ball.draw()
 
+        left_paddle.draw()
+        right_paddle.draw()
+
+        input = pygame.key.get_pressed()
+        move_paddle(input, left_paddle, right_paddle)
         ball.move()
-        collision(ball, left_paddle, right_paddle)
+
+        hit_paddle(ball, left_paddle, right_paddle)
 
 if __name__ == "__main__":
     main()
