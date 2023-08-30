e8a652e25b0237820c1d05c68e88540475c893ee
MoonS <msuyoung5@gmail.com>
2023-08-31 00:36:39 +0800

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,18 +1,21 @@
 import pygame
+import math
 from sys import exit
 
 pygame.init()
+pygame.font.init()
 WIDTH, HEIGHT = 640, 480
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Pong')
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 50)
 
 class Paddle:
     def __init__(self, screen, x, y):
         self.screen = screen
-        self.x = x
-        self.y = y
-        self.velocity = 5
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.velocity = 6
 
     def draw(self):
         pygame.draw.rect(self.screen, (255 ,255, 255), (self.x, self.y, 10, 80))
@@ -22,13 +25,17 @@ class Paddle:
             self.y -= self.velocity
         else:
             self.y += self.velocity
+    
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
 
 class Ball:
     def __init__(self, screen, x, y, radius):
         self.screen = screen
-        self.x = x
-        self.y = y
-        self.x_vel = 7
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.x_vel = 4
         self.y_vel = 0
         self.radius = radius
 
@@ -38,6 +45,12 @@ class Ball:
     def move(self):
         self.x += self.x_vel
         self.y += self.y_vel
+    
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.x_vel *= -1
+        self.y_vel = 0
 
 def move_paddle(input, left_paddle, right_paddle):
     if input[pygame.K_w] and left_paddle.y - left_paddle.velocity >= 0:
@@ -51,18 +64,30 @@ def move_paddle(input, left_paddle, right_paddle):
         right_paddle.move(up=False)
 
 def hit_paddle(ball, left_paddle, right_paddle):
-    if ball.x_vel < 0 and ball.x - ball.radius <= left_paddle.x + 10 and left_paddle.y <= ball.y <= left_paddle.y + 80:
-        ball.x_vel *= -1
-    elif ball.x_vel > 0 and ball.x + ball.radius >= right_paddle.x and right_paddle.y <= ball.y <= right_paddle.y + 80:
-        ball.x_vel *= -1
+    if ball.x - ball.radius <= left_paddle.x + 10 and left_paddle.y <= ball.y <= left_paddle.y + 80:
+        bounce(ball, left_paddle)
+    elif ball.x + ball.radius >= right_paddle.x and right_paddle.y <= ball.y <= right_paddle.y + 80:
+        bounce(ball, right_paddle)
 
     if ball.y - ball.radius <= 0 or ball.y + ball.radius >= HEIGHT:
         ball.y_vel *= -1
 
+def bounce(ball, paddle):
+    relative_d = (paddle.y + 40) - ball.y
+    bounce_angle = (relative_d/40) * ((math.pi)/3)
+    if ball.x_vel < 0:
+        ball.x_vel = 8*math.cos(bounce_angle)
+        ball.y_vel = -8*math.sin(bounce_angle)
+    else:
+        ball.x_vel = -8*math.cos(bounce_angle)
+        ball.y_vel = -8*math.sin(bounce_angle)
+
 def main():
-    left_paddle = Paddle(SCREEN, 15, HEIGHT//2 - 40)
-    right_paddle = Paddle(SCREEN, WIDTH - 25, HEIGHT//2 - 40)
+    left_paddle = Paddle(SCREEN, 10, HEIGHT//2 - 40)
+    right_paddle = Paddle(SCREEN, WIDTH - 20, HEIGHT//2 - 40)
     ball = Ball(SCREEN, WIDTH//2, HEIGHT//2, 7)
+    left_score = 0
+    right_score = 0
 
     while True:
         clock.tick(60)
@@ -73,9 +98,13 @@ def main():
                 pygame.quit()
                 exit()
 
-        SCREEN.fill((0, 0, 0))
+        SCREEN.fill((30, 30, 30))
+        left_score_txt = font.render(f"{left_score}", True, (255, 255, 255))
+        right_score_txt = font.render(f"{right_score}", True, (255, 255, 255))
+        SCREEN.blit(left_score_txt, (WIDTH//4 - left_score_txt.get_width()//2, 20))
+        SCREEN.blit(right_score_txt, (WIDTH*3//4 + right_score_txt.get_width()//2, 20))
+        pygame.draw.line(SCREEN, (80, 80, 80), (WIDTH//2, 0), (WIDTH//2, HEIGHT), width=3)
         ball.draw()
-
         left_paddle.draw()
         right_paddle.draw()
 
@@ -84,6 +113,17 @@ def main():
         ball.move()
 
         hit_paddle(ball, left_paddle, right_paddle)
+
+        if ball.x <= 0:
+            right_score += 1
+            ball.reset()
+            left_paddle.reset()
+            right_paddle.reset()
+        elif ball.x >= WIDTH:
+            left_score += 1
+            ball.reset()
+            left_paddle.reset()
+            right_paddle.reset()
 
 if __name__ == "__main__":
     main()
